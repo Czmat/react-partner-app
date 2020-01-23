a79e88c5700a5341ec4cf4007a8990f034b5ca70
Erika Hilty <erika.hilty@rfsmart.com>
2020-01-22 19:56:13 -0500

    added range value

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
+import InputRange from 'react-input-range';
 
 function App() {
+  const [rangeValue, setRangeValue] = useState({min: 0, max: 255});
+  const redValue = 0;
+  const blueValue = 0;
+  const greenValue = 0;
+
+  function onChange() {
+    
+  } 
+
   return (
     <div className="container">
       <div className="form">
@@ -9,22 +19,33 @@ function App() {
           <div>
             <label>
               <input
+                min="0"
+                max="255"
                 onChange={e => console.log(e.target.value)}
                 className="red"
                 type="range"
               />
-              red {0}
+              red {rangeValue.max}
             </label>
           </div>
           <div>
             <label>
-              <input className="green" type="range" />
+              <input 
+              min="0"
+              max="255"
+              onChange={e => console.log(e.target.value)}
+              className="green" 
+              type="range" />
               green {0}
             </label>
           </div>
           <div>
             <label>
-              <input className="blue" type="range" />
+              <input 
+              min="0"
+              max="255"
+              className="blue"
+               type="range" />
               blue {0}
             </label>
           </div>
