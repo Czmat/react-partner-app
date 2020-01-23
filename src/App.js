d86f8ea0477bf71fba2c478723e95f64983a2fcb
Chaise Matev <aj_fl81@msn.com>
2020-01-22 19:35:27 -0500

    done with one input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <form>
-        <label>
-        <input className="red" type="range"/>
-        red
-        </label>
-      </form>
+    <div className="container">
+      <div className="form">
+        <form>
+          <div>
+            <label>
+              <input
+                onChange={e => console.log(e.target.value)}
+                className="red"
+                type="range"
+              />
+              red {0}
+            </label>
+          </div>
+          <div>
+            <label>
+              <input className="green" type="range" />
+              green {0}
+            </label>
+          </div>
+          <div>
+            <label>
+              <input className="blue" type="range" />
+              blue {0}
+            </label>
+          </div>
+        </form>
+      </div>
+      <div className="box"></div>
     </div>
   );
 }
