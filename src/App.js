6a124f86e0f24ddd8a41f9abf578dfa2f6272f2e
Erika Hilty <erika.hilty@rfsmart.com>
2020-01-22 20:37:13 -0500

    got state to work on the sliders

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import './App.css';
 
 function App() {
   const defaultNum = 127;
-  const [rangeValue, setRangeValue] = useState(defaultNum);
+  const colorValues = [{red: defaultNum}, {blue: defaultNum}, {green: defaultNum}]
+  const [rangeValue, setRangeValue] = useState({red: defaultNum, green: defaultNum, blue: defaultNum});
   // const redValue = 0;
   // const blueValue = 0;
   // const greenValue = 0;
 
   function onChange(e) {
+    const change = {}
+    change[e.target.name] = e.target.value
     console.log(rangeValue);
-    setRangeValue(e.target.value);
+    setRangeValue({...rangeValue, ...change});
   }
 
   return (
@@ -23,11 +26,12 @@ function App() {
               <input
                 min="0"
                 max="255"
+                name="red"
                 onChange={e => onChange(e)}
                 className="red"
                 type="range"
               />
-              red {rangeValue.min}
+              red {rangeValue.red}
             </label>
           </div>
           <div>
@@ -35,17 +39,20 @@ function App() {
               <input
                 min="0"
                 max="255"
-                onChange={e => console.log(e.target.value)}
+                onChange={e => onChange(e)}
                 className="green"
+                name="green"
                 type="range"
               />
-              green {0}
+              green {rangeValue.green}
             </label>
           </div>
           <div>
             <label>
-              <input min="0" max="255" className="blue" type="range" />
-              blue {0}
+              <input min="0" max="255" className="blue" name="blue" type="range"
+              onChange={e => onChange(e)}
+               />
+              blue {rangeValue.blue}
             </label>
           </div>
         </form>
